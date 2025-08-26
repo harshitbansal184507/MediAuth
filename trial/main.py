8767bc6fecd5880a6aafa2fe0e243a6b2af6b5ac
Harshit Bansal <84428515+harshitbansal184507@users.noreply.github.com>
2025-08-26 17:41:01 +0530

    Update main.py

--- trial/main.py
+++ trial/main.py
@@ -2,8 +2,6 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 import requests
 
-# load image from the IAM database
-url = 'https://fki.tic.heia-fr.ch/static/img/a01-122-02-00.jpg'
 image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
 
 processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
