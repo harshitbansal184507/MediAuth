36c9f59c189dbc156c139af7890321647f035696
Harshit Bansal <84428515+harshitbansal184507@users.noreply.github.com>
2025-10-04 07:03:38 +0530

    Update settings.py

--- mediauth/mediauth/settings.py
+++ mediauth/mediauth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uy$u-hh$15x+j)!lek8656uu6=_16b@r!i-7nxs-lekl%p%4mj"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
